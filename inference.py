ee5ed85f24efbaa3621b708b93cd0ecb20b82c4a
jiangminghao <jiangminghao@antiy.cn>
2019-01-08 09:08:37 +0800

    从测试集文件中读取数据

--- inference.py
+++ inference.py
@@ -2,15 +2,16 @@ __author__ = 'jmh081701'
 import  tensorflow as tf
 from  utils import  DATAPROCESS
 
-dataGen = DATAPROCESS(source_ling_path="data/cn.txt",
-                          dest_ling_path="data/en.txt",
+dataGen = DATAPROCESS(source_ling_path="data/cn.test.txt",
+                          dest_ling_path="data/en.test.txt",
                           source_word_embedings_path="data/cn.txt.ebd.npy",
                           source_vocb_path="data/cn.txt.vab",
                           dest_word_embeddings_path="data/en.txt.ebd.npy",
                           dest_vocb_path="data/en.txt.vab",
                           batch_size=300,
-                          seperate_rate=0.1
+                          seperate_rate=1,
                         )
+            #所有的test里面的样本都拿去测试,seperate_rate 于是应该是100%,表示所有的样本都分离开来了
 
 
 
@@ -25,11 +26,11 @@ with tf.Session(graph=loaded_graph) as sess:
     target_sequence_length = loaded_graph.get_tensor_by_name('target_sequence_len:0')
     source_sequence_length = loaded_graph.get_tensor_by_name('source_sequence_len:0')
     print("inference begin ")
-    output_x,output_label,src_sequence_length,dst_sequence_length=dataGen.next_test_batch()
+    output_x,output_label,src_sequence_length,dst_sequence_length=dataGen.test_data()
     print("inference")
 
     translate_logits=sess.run(fetches=logits,feed_dict={input_data:output_x,target_sequence_length:dst_sequence_length,source_sequence_length:src_sequence_length})
-    for i in range(3):
+    for i in range(30):
         src=dataGen.src_id2words(output_x[i])
         dst=dataGen.tgt_id2words(translate_logits[i])
         print({"src":src})
